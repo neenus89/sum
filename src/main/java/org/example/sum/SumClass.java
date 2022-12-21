cf2064bf633b9f6959331bb3fbf180dab6714a44
neenus89 <neenus@xminds.com>
2022-12-21 12:45:08 +0530

    added sum

--- src/main/java/org/example/sum/SumClass.java
+++ src/main/java/org/example/sum/SumClass.java
@@ -1,2 +1,12 @@
-package org.example.sum;public class SumClass {
+package org.example.sum;
+
+public class SumClass{
+    public  static void main(String [] arg){
+        int sum = 0;
+        int i;
+        for(i = 0; i <= 10; i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
 }
